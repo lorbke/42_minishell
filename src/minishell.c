f122ba26a7ef9fcf961fecbd0fbca5c47a85392a
Luca Orbke <luca.orbke@brinkjost.de>
2022-12-09 20:34:08 +0100

    implemented prompt loop with readline history

--- src/minishell.c
+++ src/minishell.c
@@ -6,15 +6,40 @@
 /*   By: lorbke <lorbke@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/09 16:50:40 by lorbke            #+#    #+#             */
-/*   Updated: 2022/12/09 20:32:46 by lorbke           ###   ########.fr       */
+/*   Updated: 2022/12/09 20:33:42 by lorbke           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include "libft.h"
+
+#define PROMPT "minishell> "
+
+/* Free this 
+	free(line);
+	rl_clear_history();
+*/
+
+/* Read-Eval-Print-Loop */
+void	ms_rep_loop(void)
+{
+	char	*line;
+
+	line = ft_calloc(LINE_MAX, sizeof(char));
+	while (1)
+	{
+		line = readline(PROMPT);
+		if (*line)
+			add_history(line);
+	}
+}
 
 int	main(void)
 {
-	printf("Welcome to minishell!\n");
+	ms_rep_loop();
 	return (0);
 }
