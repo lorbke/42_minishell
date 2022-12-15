6c990d3cd7b01abefc64606f05d6969a57e1be58
Luca Orbke <luca.orbke@brinkjost.de>
2022-12-15 19:57:10 +0100

    changed wording of description to more accuratly match the function name

--- lib/libft/src/adds/ft_strsep.c
+++ lib/libft/src/adds/ft_strsep.c
@@ -6,7 +6,7 @@
 /*   By: lorbke <lorbke@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 15:26:56 by lorbke            #+#    #+#             */
-/*   Updated: 2022/12/15 15:24:34 by lorbke           ###   ########.fr       */
+/*   Updated: 2022/12/15 19:56:10 by lorbke           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,10 @@ static char	*set_stringp(char **stringp, char *start, char *end)
 	return (start);
 }
 
-/* This function tokenizes a string using a set of delimiters by
-replacing the first occurence of a delim char with \0, returning the thereby
+/* This function tokenizes a string using a set of separators (delimiters) by
+replacing the first occurence of a separator char with \0, returning the thereby
 created substring and moving the stringp pointer to the next character after
-the found delim char. */
+the found separator char. */
 char	*ft_strsep(char **stringp, const char *delim)
 {
 	char	*start;
