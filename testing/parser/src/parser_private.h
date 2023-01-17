3400191a68524ea7dd2ff6348556fb6e8ff23394
Luca <luca@calculator.fritz.box>
2023-01-17 23:23:05 +0100

    did some cosmetics

--- testing/parser/src/parser_private.h
+++ testing/parser/src/parser_private.h
@@ -16,8 +16,8 @@
 # include "lexer.h" // t_token, t_stack
 # include "parser.h" // t_ast
 
-t_ast	*append_left_ast(t_ast *main, t_ast *append);
 t_ast	*create_ast_node(t_token	*token);
+t_ast	*append_left_ast(t_ast *main, t_ast *append);
 t_ast	*rule_simple_cmd(t_stack **tokstack);
 t_ast	*rule_comp_cmd(t_stack **tokstack);
 t_ast	*rule_pipeline(t_stack **tokstack);
