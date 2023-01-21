faf91a46246bd954328450ba2068eec08ec51bcb
Luca Orbke <luca.orbke@brinkjost.de>
2023-01-21 18:55:56 +0100

    made the debugger prettier

--- src/debugger/debugger.c
+++ src/debugger/debugger.c
@@ -6,7 +6,7 @@
 /*   By: lorbke <lorbke@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/14 15:14:19 by lorbke            #+#    #+#             */
-/*   Updated: 2023/01/20 17:38:37 by lorbke           ###   ########.fr       */
+/*   Updated: 2023/01/21 18:53:50 by lorbke           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,6 @@ void	print_tokstack(t_stack *head)
 	t_stack	*temp;
 
 	temp = head;
-	printf(BLUE "\n=========Token Stack=========\n" RESET);
 	printf("stack: ");
 	while (head)
 	{
@@ -37,13 +36,10 @@ void	print_tokstack(t_stack *head)
 		printf("[ %*u ]->", strlen(temp->token->word), temp->token->desc);
 		temp = temp->next;
 	}
-	printf("\n");
 }
 
 void	print_ast(t_ast *ast, int width)
 {
-	if (!width)
-		printf(BLUE "\n=========Abstract Syntax Tree=========\n" RESET);
 	if (!ast)
 		return ;
 	print_ast(ast->right, width + 7);
@@ -56,30 +52,35 @@ void	debug_print_termios(struct termios *termios)
 {
 	if (!DEBUG)
 		return ;
-	printf("termios:\n");
-	printf("	input mode: %lx\n", termios->c_iflag);
-	printf("	output mode: %lx\n", termios->c_oflag);
-	printf("	control mode: %lx\n", termios->c_cflag);
-	printf("	local mode: %lx\n", termios->c_lflag);
-	printf("	control characters: %hhu\n", termios->c_cc[VQUIT]);
-	printf("	input speed: %lu\n", termios->c_ispeed);
-	printf("	output speed: %lu\n", termios->c_ospeed);
+	printf(BLUE "\n=========Termios=========\n" RESET);
+	printf("input mode:    %lx\n", termios->c_iflag);
+	printf("output mode:   %lx\n", termios->c_oflag);
+	printf("control mode:  %lx\n", termios->c_cflag);
+	printf("local mode:    %lx\n", termios->c_lflag);
+	printf("control chars: %hhu\n", termios->c_cc[VQUIT]);
+	printf("input speed:   %lu\n", termios->c_ispeed);
+	printf("output speed:  %lu\n", termios->c_ospeed);
+	printf(BLUE "=========================\n\n\n" RESET);
 }
 
 void	debug_lexer(t_stack *tokstack)
 {
 	if (!DEBUG)
 		return ;
+	printf(BLUE "\n=========Token Stack=========\n" RESET);
 	print_tokstack(tokstack);
+	printf(BLUE "\n=============================\n\n\n" RESET);
 }
 
 void	debug_parser(t_ast *ast, t_stack *tokstack)
 {
 	if (!DEBUG)
 		return ;
+	printf(BLUE "\n=========Abstract Syntax Tree=========\n" RESET);
 	print_ast(ast, 0);
 	if (tokstack)
 		printf(RED "minishell: syntax error near unexpected token `%s'\n" RESET, tokstack->token->word);
 	else
 		printf(GREEN "minishell: syntax valid!\n" RESET);
+	printf(BLUE "======================================\n\n\n" RESET);
 }
