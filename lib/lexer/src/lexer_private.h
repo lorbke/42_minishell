4a55dbd7f6358acf2b2f1d65762de2903dd646cf
Luca Orbke <luca.orbke@brinkjost.de>
2023-02-12 18:16:01 +0100

    fixed missing separator edge case

--- lib/lexer/src/lexer_private.h
+++ lib/lexer/src/lexer_private.h
@@ -6,14 +6,15 @@
 /*   By: lorbke <lorbke@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 18:15:38 by lorbke            #+#    #+#             */
-/*   Updated: 2023/01/18 15:16:53 by lorbke           ###   ########.fr       */
+/*   Updated: 2023/02/12 18:11:48 by lorbke           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LEXER_PRIVATE_H
 # define LEXER_PRIVATE_H
 
-// TODO: change overly complicated and ugly handling of parentheses
+// @todo change overly complicated and ugly handling of parentheses
 char	*lexer_ft_strsep(char **stringp, const char *delim, const char *ignore);
+char	*insert_sep_around_special(char *src, char *seps);
 
 #endif
