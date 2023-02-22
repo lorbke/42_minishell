06c10a7f371e655f23381312c0329a17c6ce3b86
lorbke <luca.orbke@brinkjost.de>
2023-02-22 22:41:35 +0100

    fixed bash double syntax error

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: lorbke <lorbke@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/09 16:50:40 by lorbke            #+#    #+#             */
-/*   Updated: 2023/02/22 22:10:17 by lorbke           ###   ########.fr       */
+/*   Updated: 2023/02/22 22:32:08 by lorbke           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,7 +42,7 @@
 // @todo heredoc and doc read switch to get_next_line?
 // @todo add g_sym_table to garbage collector
 
-// @todo env -i ./minishell segfaults
+// @todo env -i ./minishell leaks?
 // @todo << "lim" -> no expansion
 // @todo expansion in heredoc and infiles, except if lim in quotes
 // @todo heredoc lim itself is not expanded
