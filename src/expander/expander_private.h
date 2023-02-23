d908962b9b601ddb3a07fed47a334439cccfd51b
Fio <fyuzhyk@student.42heilbronn.de>
2023-02-23 13:25:07 +0100

    fixxed quote removal logic / added skip_quotes_in_quotes

--- src/expander/expander_private.h
+++ src/expander/expander_private.h
@@ -6,7 +6,7 @@
 /*   By: fyuzhyk <fyuzhyk@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/10 08:33:05 by fyuzhyk           #+#    #+#             */
-/*   Updated: 2023/02/22 23:33:14 by fyuzhyk          ###   ########.fr       */
+/*   Updated: 2023/02/23 13:20:15 by fyuzhyk          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ char	*add_char_to_str(char *result, char c, int *index, int *result_index);
 // quotes
 char	*handle_quotes(char *result, char *str, int *index, int *result_index);
 void	quote_removal(char **argv);
+void	skip_quotes_in_quotes(char *str, int *i, int *j);
 int		find_closing_quote(char *str, int *index, char quote_type);
 int		in_closed_quotes(char *result, char *arg, int *index, int *result_index);
 
