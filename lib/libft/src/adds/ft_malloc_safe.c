13cb567f8b03f0ae94cd13b5238f918db6220d63
lorbke <luca.orbke@brinkjost.de>
2023-02-25 00:17:58 +0100

    added perror_and_exit function

--- lib/libft/src/adds/ft_malloc_safe.c
+++ lib/libft/src/adds/ft_malloc_safe.c
@@ -6,13 +6,12 @@
 /*   By: lorbke <lorbke@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/24 23:58:40 by lorbke            #+#    #+#             */
-/*   Updated: 2023/02/25 00:07:21 by lorbke           ###   ########.fr       */
+/*   Updated: 2023/02/25 00:17:35 by lorbke           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h" // ft_perror_and_exit
 #include <stdlib.h> // malloc, exit, size_t
-#include <stdio.h> // perror
-#include <sys/errno.h> // errno
 
 /* Wrapper function for malloc that checks if allocation size is above zero and
 prints an error message and calls exit if allocation failed. */
@@ -24,9 +23,6 @@ void	*ft_malloc_safe(size_t size, size_t count)
 		return (NULL);
 	ptr = malloc(size * count);
 	if (ptr == NULL)
-	{
-		perror("malloc: ");
-		exit(errno);
-	}
+		ft_perror_and_exit("malloc: ");
 	return (ptr);
 }
---
+++ lib/libft/src/adds/ft_perror_and_exit.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_perror_and_exit.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lorbke <lorbke@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/25 00:14:58 by lorbke            #+#    #+#             */
+/*   Updated: 2023/02/25 00:15:54 by lorbke           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h> // exit
+#include <stdio.h> // perror
+#include <sys/errno.h> // errno
+
+/* Prints an error message and calls exit with errno. */
+void	ft_perror_and_exit(char *msg)
+{
+	perror(msg);
+	exit(errno);
+}
